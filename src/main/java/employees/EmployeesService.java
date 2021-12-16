5b9a4e0ae60309f189bffde642f95957d90a6638
Kovács Botond <kovacs.botond1@telekom.hu>
2021-12-16 15:50:30 +0100

    jpa

--- src/main/java/employees/EmployeesService.java
+++ src/main/java/employees/EmployeesService.java
@@ -1,16 +1,13 @@
 package employees;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
+import javax.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,42 +16,45 @@ import org.springframework.stereotype.Service;
 public class EmployeesService {
 
 	private ModelMapper modelMapper;
-	private EmployeeDao employeeDao;
+	private EmployeesRepository repository;
 
 
 	public List<EmployeeDto> listEmployees(Optional<String> prefix) {
 		var targetListType = new TypeToken<List<EmployeeDto>>() {
 		}.getType();
-		var filteredEmployees = employeeDao.findALL().stream()
+		var filteredEmployees = repository.findAll().stream()
 				.filter(employee -> prefix.isEmpty() || employee.getName().toLowerCase().startsWith(prefix.get().toLowerCase()))
 				.collect(Collectors.toList());
 		return modelMapper.map(filteredEmployees, targetListType);
 	}
 
 	public EmployeeDto listEmployeeById(long id) {
-		return modelMapper.map(employeeDao.findById(id), EmployeeDto.class);
+		return modelMapper.map(repository.findById(id)
+						.orElseThrow(() -> new IllegalArgumentException("employee not found"))
+				, EmployeeDto.class);
 	}
 
 	public EmployeeDto createEmployee(CreateEmployeeCommand command) {
 		var employee = new Employee(command.getName());
-		employeeDao.createEmployee(employee);
+		repository.save(employee);
 		log.info("Employee has been created");
 		log.debug(String.format("Employee has been created, with name: %s id: %s", employee.getName(), employee.getId()));
 		return modelMapper.map(employee, EmployeeDto.class);
 	}
 
+	@Transactional
 	public EmployeeDto updateEmployee(long id, UpdateEmployeeCommand command) {
-		var employee = new Employee(id,command.getName());
-		employeeDao.updateEmployee(employee);
+		var employee = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("employee not found"));
+		employee.setName(command.getName());
 		return modelMapper.map(employee, EmployeeDto.class);
 	}
 
 	public void deleteEmployee(long id) {
-		employeeDao.deleteById(id);
+		repository.deleteById(id);
 	}
 
 	public void deleteAllEmployees() {
-		employeeDao.deleteAll();
+		repository.deleteAll();
 	}
 
 
